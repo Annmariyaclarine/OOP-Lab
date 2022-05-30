3a812dec639ebd727e00a46668539dcbf2f24476
Annmariyaclarine <95731201+Annmariyaclarine@users.noreply.github.com>
2022-05-30 11:55:18 +0530

    Add files via upload

---
+++ NestedTry.java
@@ -0,0 +1,34 @@
+package s2java;
+
+public class NestedTry {
+
+	 public static void main(String args[]) {
+	     int number[]={20,40,60,80,100};
+	     int denom[]={5,0,0,10};
+	     try //outer
+	      {
+	         for(int i=0;i<number.length;i++)
+	           {
+	             try//nested try
+	               {
+	                 int q=number[i]/denom[i];
+	                 System.out.println(number[i]+"/"+denom[i]+"=" + q);
+	               }
+	             catch(ArithmeticException e) 
+	               {
+	                  System.out.println("Divide by 0: " + e);
+	               } 
+	             catch(ArrayIndexOutOfBoundsException e)
+	               {
+	                 System.out.println("Array index oob: " + e);
+	                  System.out.println("Program terminated.");
+
+	               }
+	             
+	            }
+	       }
+	      finally
+	       { System.out.println("In final block.");}
+
+	  }
+}
